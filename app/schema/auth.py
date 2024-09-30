0ea4f7f2bb2d91432a0868bd52396fcca0e5ffb3
nuru <0xnuru@gmail.com>
2024-09-30 09:35:53 +0100

    feat: add auth schema

---
+++ app/schema/auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from pydantic import BaseModel
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+    role: str
+
+    class Config:
+        from_attributes = True
